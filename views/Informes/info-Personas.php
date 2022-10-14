390bb403084a6481c7ae281ac48b4957c7d2fee5
Smoki26 <enzo.ledezma@hotmail.com>
2022-10-14 15:36:38 -0300

    update

--- views/Informes/info-Personas.php
+++ views/Informes/info-Personas.php
@@ -178,7 +178,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
                                   </div>';
                                     }
                                       ?>
-                              
                                   </div>
 
 
